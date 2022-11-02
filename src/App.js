26d19b4870c83807379f2f156d30a0f47feb0755
csilvestro <dixie15499@yahoo.com>
2022-11-02 11:17:19 -0400

    Adding dropdown

--- src/App.js
+++ src/App.js
@@ -1,58 +1,107 @@
 import * as React from 'react';
 
 function App() {
-  const [food, setFood] = React.useState('fruit');
-  const [drink, setDrink] = React.useState('water');
+  const [open, setOpen] = React.useState(false);
 
-  const handleFoodChange = (event) => {
-    setFood(event.target.value);
+  const handleOpen = () => {
+    setOpen(!open);
   };
 
-  const handleDrinkChange = (event) => {
-    setDrink(event.target.value);
+  const handleMenuOne = () => {
+    // do something
+    setOpen(false);
+    console.log('clicked one');
   };
+
+  const handleMenuTwo = () => {
+    // do something
+    setOpen(false);
+    console.log('clicked two');
+  };
+
 
 return (
   <div>
-    <p>Select using Hooks</p>
-    <Select
-        label="What do we eat?"
-        options={[
-          { label: 'Fruit', value: 'fruit' },
-          { label: 'Vegetable', value: 'vegetable' },
-          { label: 'Meat', value: 'meat' },
-        ]}
-        value={food}
-        onChange={handleFoodChange}
-      />
-
-      <Select
-        label="What do we drink?"
-        options={[
-          { label: 'Water', value: 'water' },
-          { label: 'Beer', value: 'beer' },
-          { label: 'Wine', value: 'wine' },
-        ]}
-        value={drink}
-        onChange={handleDrinkChange}
-      />
-
-      <p>We eat {food}!</p>
-      <p>We drink {drink}!</p>
+    <p>Drop Down using Hooks</p>
+    <Dropdown
+      open={open}
+      trigger={<button>Dropdown</button>}
+      menu={[
+        <button onClick={handleMenuOne}>Menu 1</button>,
+        <button onClick={handleMenuTwo}>Menu 2</button>,
+      ]}
+    />
     </div>
-)}
+  );
+};
 
-const Select = ({ label, value, options, onChange }) => {
+const Dropdown = ({ trigger, menu }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <label>
-      {label}
-      <select value={value} onChange={onChange}>
-        {options.map((option) => (
-          <option value={option.value}>{option.label}</option>
-        ))}
-      </select>
-    </label>
+    <div className="dropdown">
+      {React.cloneElement(trigger, {
+        onClick: handleOpen,
+      })}
+      {open ? (
+        <ul className="menu">
+          {menu.map((menuItem, index) => (
+            <li key={index} className="menu-item">
+              {React.cloneElement(menuItem, {
+                onClick: () => {
+                  menuItem.props.onClick();
+                  setOpen(false);
+                },
+              })}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
   );
 };
 
 export default App;
+
+const styles = {
+  dropdown: {
+    position: 'relative'
+  },
+  menu: {
+    position: 'absolute',
+    list-style-type: 'none',
+    margin: '5px 0',
+    padding: 0,
+    border: '1px solid grey',
+    width: '150px'
+  }
+}
+
+
+.menu > li {
+  margin: 0;
+
+  background-color: white;
+}
+
+.menu > li:hover {
+  background-color: lightgray;
+}
+
+.menu > li > button {
+  width: 100%;
+  height: 100%;
+  text-align: left;
+
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 5px;
+  margin: 0;
+  font: inherit;
+  cursor: pointer;
+}
